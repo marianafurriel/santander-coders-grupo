7bc26fcf74757db2662196e5bfdcb415d12fc9a7
Mariana Furriel <marianafurrielss@gmail.com>
2023-09-02 17:41:53 -0300

    :lipstick: feat: Estilizaçao CSS do tabuleiro suavizada

--- controleCSS.js
+++ controleCSS.js
@@ -1,6 +1,21 @@
 const botoes = [...document.querySelectorAll(".botao")];
 
 botoes.forEach((botao) =>
+  botao.addEventListener("mouseenter", () => {
+    const cor = getComputedStyle(botao).backgroundColor;
+    botao.style.boxShadow = `0px 0px 10px 1px ${cor}`;
+  })
+);
+
+botoes.forEach((botao) =>
+  botao.addEventListener("mouseleave", () => {
+    setTimeout(function () {
+      botao.style.boxShadow = "";
+    }, 100);
+  })
+);
+
+botoes.forEach((botao) =>
   botao.addEventListener("mousedown", () => {
     const cor = getComputedStyle(botao).backgroundColor;
     botao.style.boxShadow = `0px 0px 60px 4px ${cor}`;
@@ -10,7 +25,8 @@ botoes.forEach((botao) =>
 botoes.forEach((botao) =>
   botao.addEventListener("mouseup", () => {
     setTimeout(function () {
-      botao.style.boxShadow = "";
+      const cor = getComputedStyle(botao).backgroundColor;
+      botao.style.boxShadow = `0px 0px 10px 1px ${cor}`;
     }, 100);
   })
 );
